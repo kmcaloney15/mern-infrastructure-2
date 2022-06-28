9261a67d519c7fc57511ead821d269fb8cc3878c
kmcaloney15 <katymcaloney@gmail.com>
2022-06-28 13:11:49 -0400

    added log out function

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,8 +17,17 @@
 import './NavBar.css'
 import { Link } from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
+import * as userService from '../../utilities/users-service'
 
 export default function NavBar({ user }) {
+  function handleLogOut(){
+    //Delegate to the users-service modle
+    userService.logOut()
+    // Update state will also cause a re-render
+    setUser(null);
+
+  }
+
     return(
         <nav>
             <Link to="/orders">Order History</Link>
@@ -26,6 +35,8 @@ export default function NavBar({ user }) {
             <Link to="/orders/new">New Order</Link>
             &nbsp; | &nbsp;
             Welcome, { user.name }
+            &nbsp; | &nbsp;
+            <Link to="" onClick={handleLogOut}>Log Out</Link>
         </nav>
     )
 }
